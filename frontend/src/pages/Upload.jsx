a2d8c5cba03ea11f80b58ffb9bbcaff86b49eb2b
bharatsingh01102005 <bharatsingh01102005@gmail.com>
2025-11-21 17:16:01 +0530

    small correction

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -6,7 +6,7 @@ import { FiPlusSquare } from "react-icons/fi";
 import { useRef } from 'react';
 import VideoPlayer from '../components/VideoPlayer';
 import axios from 'axios';
-import { serverUrl } from '../App';
+import { serverUrl } from '../api';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPostData } from '../redux/postSlice';
 import { setCurrentUserStory, setStoryData } from '../redux/storySlice';
@@ -19,13 +19,13 @@ function Upload() {
     const [frontendMedia, setFrontendMedia] = useState(null)
     const [backendMedia, setBackendMedia] = useState(null)
     const [mediaType, setMediaType] = useState("")
-    const [caption,setCaption]=useState("")
+    const [caption, setCaption] = useState("")
     const mediaInput = useRef()
-    const dispatch=useDispatch()
-    const {postData}=useSelector(state=>state.post)
-     const {storyData}=useSelector(state=>state.story)
-      const {loopData}=useSelector(state=>state.loop)
-      const [loading,setLoading]=useState(false)
+    const dispatch = useDispatch()
+    const { postData } = useSelector(state => state.post)
+    const { storyData } = useSelector(state => state.story)
+    const { loopData } = useSelector(state => state.loop)
+    const [loading, setLoading] = useState(false)
     const handleMedia = (e) => {
         const file = e.target.files[0]
         console.log(file)
@@ -38,59 +38,59 @@ function Upload() {
         setFrontendMedia(URL.createObjectURL(file))
     }
 
-const uploadPost=async ()=>{
-   
-    try {
-        const formData=new FormData()
-        formData.append("caption",caption)
-        formData.append("mediaType",mediaType)
-        formData.append("media",backendMedia)
-        const result=await axios.post(`${serverUrl}/api/post/upload`,formData,{withCredentials:true})
-       dispatch(setPostData([...postData,result.data]))
-       setLoading(false)
-       navigate("/")
-    } catch (error) {
-        console.log(error)
-    }
-}
+    const uploadPost = async () => {
 
-const uploadStory=async ()=>{
-    try {
-        const formData=new FormData()
-        formData.append("mediaType",mediaType)
-        formData.append("media",backendMedia)
-        const result=await axios.post(`${serverUrl}/api/story/upload`,formData,{withCredentials:true})
-       dispatch(setCurrentUserStory(result.data))
-         setLoading(false)
-       navigate("/")
-    } catch (error) {
-        console.log(error)
+        try {
+            const formData = new FormData()
+            formData.append("caption", caption)
+            formData.append("mediaType", mediaType)
+            formData.append("media", backendMedia)
+            const result = await axios.post(`${serverUrl}/api/post/upload`, formData, { withCredentials: true })
+            dispatch(setPostData([...postData, result.data]))
+            setLoading(false)
+            navigate("/")
+        } catch (error) {
+            console.log(error)
+        }
     }
-}
-const uploadLoop=async ()=>{
-    try {
-        const formData=new FormData()
-        formData.append("caption",caption)
-        formData.append("media",backendMedia)
-        const result=await axios.post(`${serverUrl}/api/loop/upload`,formData,{withCredentials:true})
-         dispatch(setLoopData([...loopData,result.data]))
-         setLoading(false)
-       navigate("/")
-    } catch (error) {
-        console.log(error)
-    }
-}
 
-const handleUpload=()=>{
-    setLoading(true)
-    if(uploadType=="post"){
-        uploadPost()
-    }else if(uploadType=="story"){
-        uploadStory()
-    }else{
-        uploadLoop()
+    const uploadStory = async () => {
+        try {
+            const formData = new FormData()
+            formData.append("mediaType", mediaType)
+            formData.append("media", backendMedia)
+            const result = await axios.post(`${serverUrl}/api/story/upload`, formData, { withCredentials: true })
+            dispatch(setCurrentUserStory(result.data))
+            setLoading(false)
+            navigate("/")
+        } catch (error) {
+            console.log(error)
+        }
     }
-}
+    const uploadLoop = async () => {
+        try {
+            const formData = new FormData()
+            formData.append("caption", caption)
+            formData.append("media", backendMedia)
+            const result = await axios.post(`${serverUrl}/api/loop/upload`, formData, { withCredentials: true })
+            dispatch(setLoopData([...loopData, result.data]))
+            setLoading(false)
+            navigate("/")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleUpload = () => {
+        setLoading(true)
+        if (uploadType == "post") {
+            uploadPost()
+        } else if (uploadType == "story") {
+            uploadStory()
+        } else {
+            uploadLoop()
+        }
+    }
 
     return (
         <div className='w-full h-[100vh] bg-black flex flex-col items-center '>
@@ -109,30 +109,30 @@ const handleUpload=()=>{
             </div>
 
             {!frontendMedia && <div className='w-[80%] max-w-[500px] h-[250px] bg-[#0e1316] border-gray-800 border-2 flex flex-col items-center justify-center gap-[8px] mt-[15vh] rounded-2xl cursor-pointer hover:bg-[#353a3d]' onClick={() => mediaInput.current.click()}>
-                <input type="file" accept={uploadType=="loop"?"video/*":""} hidden ref={mediaInput} onChange={handleMedia} />
+                <input type="file" accept={uploadType == "loop" ? "video/*" : ""} hidden ref={mediaInput} onChange={handleMedia} />
                 <FiPlusSquare className='text-white cursor-pointer w-[25px] h-[25px]' />
                 <div className='text-white text-[19px] font-semibold'>Upload {uploadType}</div>
             </div>}
 
             {frontendMedia &&
                 <div className='w-[80%] max-w-[500px] h-[250px]  flex flex-col items-center justify-center  mt-[15vh]'>
-             {mediaType=="image" && <div className='w-[80%] max-w-[500px] h-[250px]  flex flex-col items-center justify-center  mt-[5vh] '>
-                <img src={frontendMedia} alt="" className='h-[60%] rounded-2xl'/>
-                {uploadType!="story" &&  <input type='text' className='w-full border-b-gray-400 border-b-2 outline-none px-[10px] py-[5px] text-white mt-[20px]' placeholder='write caption' onChange={(e)=>setCaption(e.target.value)} value={caption}/>}
-               
+                    {mediaType == "image" && <div className='w-[80%] max-w-[500px] h-[250px]  flex flex-col items-center justify-center  mt-[5vh] '>
+                        <img src={frontendMedia} alt="" className='h-[60%] rounded-2xl' />
+                        {uploadType != "story" && <input type='text' className='w-full border-b-gray-400 border-b-2 outline-none px-[10px] py-[5px] text-white mt-[20px]' placeholder='write caption' onChange={(e) => setCaption(e.target.value)} value={caption} />}
+
+                    </div>}
+
+                    {mediaType == "video" && <div className='w-[80%] max-w-[500px] h-[250px]  flex flex-col items-center justify-center  mt-[5vh] '>
+                        <VideoPlayer media={frontendMedia} />
+                        {uploadType != "story" && <input type='text' className='w-full border-b-gray-400 border-b-2 outline-none px-[10px] py-[5px] text-white mt-[20px]' placeholder='write caption' onChange={(e) => setCaption(e.target.value)} value={caption} />}
+
+                    </div>}
+
+
+
+
                 </div>}
-
-                 {mediaType=="video" && <div className='w-[80%] max-w-[500px] h-[250px]  flex flex-col items-center justify-center  mt-[5vh] '>
-                <VideoPlayer media={frontendMedia}/>
-                {uploadType!="story" &&  <input type='text' className='w-full border-b-gray-400 border-b-2 outline-none px-[10px] py-[5px] text-white mt-[20px]' placeholder='write caption' onChange={(e)=>setCaption(e.target.value)} value={caption}/>}
-               
-                </div>}
-
-
-               
-
-                </div>}
-                {frontendMedia && <button className='px-[10px] w-[60%] max-w-[400px]   py-[5px] h-[50px] bg-[white] mt-[50px] cursor-pointer rounded-2xl' onClick={handleUpload}>{loading?<ClipLoader size={30} color='black'/>:`Upload ${uploadType}` }</button>}
+            {frontendMedia && <button className='px-[10px] w-[60%] max-w-[400px]   py-[5px] h-[50px] bg-[white] mt-[50px] cursor-pointer rounded-2xl' onClick={handleUpload}>{loading ? <ClipLoader size={30} color='black' /> : `Upload ${uploadType}`}</button>}
 
         </div>
     )
