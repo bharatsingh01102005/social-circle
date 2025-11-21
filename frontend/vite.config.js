a5a5ed0333d8be7f55360467b9bcb14711f7080b
bharatsingh01102005 <bharatsingh01102005@gmail.com>
2025-11-21 17:24:42 +0530

    changes in api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://social-circle-eglg.onrender.com',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:8000',
+        target: 'https://social-circle-eglg.onrender.com',
         changeOrigin: true,
       },
     },
